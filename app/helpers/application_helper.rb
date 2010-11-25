4681f2ecdcdebbf5ee2c3c9d6f31f38b50cafce2
Jonathon Jones <jonathon_d_jones@yahoo.com>
2010-11-24 21:32:21 -0500

    Removes more unused ApplicationHelper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,31 +5,4 @@ module ApplicationHelper
     @hazard_ids =  Hazard.find(:all).collect {|x| [x.name, x.id]}
   end
 
-# http://woss.name/2006/11/25/multi-select-boxes-in-your-rails-view/
-
-  def collection_select_multiple(object, method,
-                                 collection, value_method, text_method,
-                                 options = {}, html_options = {})
-    real_method = "#{method.to_s.singularize}_ids".to_sym
-    collection_select(
-      object, real_method,
-      collection, value_method, text_method,
-      options,
-      html_options.merge({
-        :multiple => true,
-        :name => "#{object}[#{real_method}][]"
-      })
-    )
-  end
-
-  def collection_select_multiple_categories(object, method,
-                                            options = {}, html_options = {})
-    collection_select_multiple(
-      object, method,
-      Category.find(:all), :id, :title,
-      options, html_options
-    )
-  end
-# /
-
 end
