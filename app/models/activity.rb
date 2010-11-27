a2a500b9cc5635359486f054a43a6c68d4c68ba5
Jonathon Jones <jonathon_d_jones@yahoo.com>
2010-11-27 01:45:25 -0500

    Changes validates_presence to person instead of person_id, removes extra code

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -3,29 +3,7 @@ class Activity < ActiveRecord::Base
   belongs_to :observation
   has_many :setups, :dependent => :destroy
   
-  validates_presence_of :person_id
+  validates_presence_of :person
   validates_associated :person
   
-  def validate
-  	if person_id.blank? or !Person.find_by_id(person_id)
-  	  errors.add(:person, "Invalid")
-  	end
-  end
-
-  # def setup_sets=(params)
-  #     # clear old setups
-  #     self.setups.clear
-  #     
-  #     #add new setups
-  #     params.each do |key, attributes|
-  #        next if attributes.delete(:deleted) == 'true'
-  #        setup = Setup.new(attributes)
-  #        self.setups << setup
-  #      end    
-  #   end
-  #   
-  #   def person_id
-  #     person ? person.id : nil
-  #   end
-  # 
 end
