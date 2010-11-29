54dcc37eea8106ea3901bb94d6e66c510ab654f6
Jonathon Jones <jonathon_d_jones@yahoo.com>
2010-11-29 13:24:27 -0500

    Since MaterialTransaction.find_fertilizations is only used in one place, it does not need much customizability

--- app/models/material_transaction.rb
+++ app/models/material_transaction.rb
@@ -8,17 +8,14 @@ class MaterialTransaction < ActiveRecord::Base
   validates_associated :material
   
   # find fertilizers  on the main site
-  def MaterialTransaction.find_fertilizations(order='obs_date desc', study_id =1)
-    MaterialTransaction.find_material_types(3,order,study_id)
-  end
-  
-  def MaterialTransaction.find_material_types(material_type_id = 1, order='obs_date desc', study_id =1)
-    areas = Area.find(:all, :conditions => ['study_id=?', study_id])
-    #    @areas = Area.find(:all, :conditions => ['study_id = 1'])
-    MaterialTransaction.find(:all, 
-    :conditions => ['materials.material_type_id = ? and area_id in (?)', material_type_id, areas], 
-    :order => order,
-    :joins => 'join materials on materials.id  = material_transactions.material_id join setups on setups.id = material_transactions.setup_id join activities on setups.activity_id = activities.id  join observations on activities.observation_id = observations.id join areas_observations on areas_observations.observation_id = observations.id join areas on areas_observations.area_id = areas.id join observation_types_observations on observation_types_observations.observation_id  =  observations.id')    
+  def MaterialTransaction.find_fertilizations(order='obs_date desc')
+    study = Study.find_by_name("MAIN")
+    material_type = MaterialType.find_by_name("K fertilizer")
+    areas = Area.find_all_by_study_id(study)
+    MaterialTransaction.find(:all,
+        :conditions => ['materials.material_type_id = ? and area_id in (?)', material_type, areas],
+        :order => order,
+        :joins => 'join materials on materials.id  = material_transactions.material_id join setups on setups.id = material_transactions.setup_id join activities on setups.activity_id = activities.id  join observations on activities.observation_id = observations.id join areas_observations on areas_observations.observation_id = observations.id join areas on areas_observations.area_id = areas.id join observation_types_observations on observation_types_observations.observation_id  =  observations.id')
   end
   
 end
