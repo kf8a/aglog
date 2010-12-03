e0873ecea1ed56d63cf19e256ceb2002834b0134
Jonathon Jones <jonathon_d_jones@yahoo.com>
2010-12-02 21:42:55 -0500

    Simplifies materials_controller

--- app/controllers/materials_controller.rb
+++ app/controllers/materials_controller.rb
@@ -1,24 +1,18 @@
 class MaterialsController < ApplicationController
+  before_filter :get_material, :only => [:show, :edit, :update, :destroy, :put_hazards]
+  respond_to :html, :xml
   
   # GET /materials
   # GET /materials.xml
   def index
     @materials = Material.find(:all, :order => 'material_type_id, name')
-    respond_to do |format|
-      format.html # index.rhtml
-      format.xml  { render :xml => @materials.to_xml }
-    end
+    respond_with @materials
   end
   
   # GET /materials/1
   # GET /materials/1.xml
   def show
-    @material = Material.find(params[:id])
-    
-    respond_to do |format|
-      format.html # show.rhtml
-      format.xml  { render :xml => @material.to_xml }
-    end
+    respond_with @material
   end
   
   # GET /materials/new
@@ -28,56 +22,31 @@ class MaterialsController < ApplicationController
   
   # GET /materials/1;edit
   def edit
-    @material = Material.find(params[:id])
+    respond_with @material
   end
 
   # POST /materials
   # POST /materials.xml
   def create
     @material = Material.new(params[:material])
-    
-    respond_to do |format|
-      if @material.save
-        flash[:notice] = 'Material was successfully created.'
-        
-        format.html { redirect_to material_url(@material) }
-        format.xml do
-          headers["Location"] = material_url(@material)
-          render :nothing => true, :status => "201 Created"
-        end
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @material.errors.to_xml }
-      end
+    if @material.save
+      flash[:notice] = 'Material was successfully created.'
     end
+    respond_with @material
   end
   
   # PUT /materials/1
   # PUT /materials/1.xml
   def update
-    @material = Material.find(params[:id])
-    
-    respond_to do |format|
-      if @material.update_attributes(params[:material])
-        format.html { redirect_to material_url(@material) }
-        format.xml  { render :nothing => true }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @material.errors.to_xml }        
-      end
-    end
+    @material.update_attributes(params[:material])
+    respond_with @material
   end
   
   # DELETE /materials/1
   # DELETE /materials/1.xml
   def destroy
-    @material = Material.find(params[:id])
     @material.destroy
-    
-    respond_to do |format|
-      format.html { redirect_to materials_url   }
-      format.xml  { render :nothing => true }
-    end
+    respond_with @material
   end
   
   # GET /materials/1/get_hazards
@@ -87,8 +56,6 @@ class MaterialsController < ApplicationController
   
   # PUT /materials/1/put_hazards
   def put_hazards
-    @material = Material.find_by_id(params[:id])
-		
     if @material.nil?
       redirect_to :action => "new"
     else
@@ -109,4 +76,10 @@ class MaterialsController < ApplicationController
 	def new_hazards
 	
 	end
+
+  private ####################
+
+  def get_material
+    @material = Material.find_by_id(params[:id])
+  end
 end
