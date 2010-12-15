12608084fc164869d06e47b8105e297596a9082e
Jonathon Jones <jonathon_d_jones@yahoo.com>
2010-12-15 15:04:13 -0500

    Tests person_sessions_controller as well as I can (create is untested)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,14 +7,14 @@ class ApplicationController < ActionController::Base
     
   helper_method :current_user, :signed_in?
 
+  def signed_in?
+    !!current_user
+  end
+
   protected
 
   def current_user
     @current_user ||= Person.find_by_id(session[:user_id])
-  end
-
-  def signed_in?
-    !!current_user
   end
 
   def current_user=(user)
--- test/functional/person_sessions_controller_test.rb
+++ test/functional/person_sessions_controller_test.rb
@@ -1,8 +1,38 @@
 require 'test_helper'
 
 class PersonSessionsControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+
+  context "GET :new" do
+    setup do
+      get :new
+    end
+
+    should render_template 'new'
+  end
+
+  context "GET :new with failed connection" do
+    setup do
+      get :new, :message => "connection_failed"
+    end
+
+    should render_template 'new'
+    should set_the_flash
+  end
+
+  context "A user is logged in. " do
+    setup do
+      sign_in_as_normal_user
+      assert @controller.signed_in?
+    end
+
+    context "DELETE :destory the session" do
+      setup do
+        delete :destroy
+      end
+
+      should "log out the user" do
+        refute @controller.signed_in?
+      end
+    end
   end
 end
