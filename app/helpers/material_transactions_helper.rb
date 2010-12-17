a8e3a1182de3550168a45b988fcf8982bf06ca8f
Jonathon Jones <jonathon_d_jones@yahoo.com>
2010-12-16 19:21:02 -0500

    Moves methods from observations_helper to material_transactions_helper where they will be used.

--- app/helpers/material_transactions_helper.rb
+++ app/helpers/material_transactions_helper.rb
@@ -1,2 +1,11 @@
 module MaterialTransactionsHelper
+
+  def materials
+    @materials ||= Material.order('material_type_id, name').collect {|x| [x.name, x.id]}
+  end
+
+  def units
+    @units ||= Unit.order('name').collect {|x| [x.name, x.id]}
+  end
+
 end
--- app/helpers/observations_helper.rb
+++ app/helpers/observations_helper.rb
@@ -1,20 +1,12 @@
 module ObservationsHelper
 
-  def initializeArrays
-     data = "arrayActivityIndexes = []; arraySetupIndexes=[];"
-     javascript_tag(data)
-   end
+  def initialize_arrays
+    data = "arrayActivityIndexes = []; arraySetupIndexes=[];"
+    javascript_tag(data)
+  end
    
-   def units
-     @units ||= Unit.find(:all, :order => 'name').collect {|x| [x.name, x.id]}
-   end
-
-   def materials
-     @materials ||= Material.find(:all, :order => 'material_type_id, name').collect {|x| [x.name, x.id]}
-   end
-   
-   def observation_type_ids
-     @observation_type_ids ||= ObservationType.find(:all).collect {|x| [x.name, x.id]}
-   end
+  def observation_type_ids
+    @observation_type_ids ||= ObservationType.all.collect {|x| [x.name, x.id]}
+  end
    
 end
