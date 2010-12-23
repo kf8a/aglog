2c905590634c99c054b9842fdd9eebb226e2d42a
Jonathon Jones <jonathon_d_jones@yahoo.com>
2010-12-22 23:41:32 -0500

    Also remove unnecessary parts of controllers

--- app/controllers/material_transactions_controller.rb
+++ app/controllers/material_transactions_controller.rb
@@ -1,10 +1,5 @@
 class MaterialTransactionsController < ApplicationController
 
-  def new
-    setup = Setup.find_by_id(params[:setup_id])
-    @transaction = if setup then setup.material_transactions.new else MaterialTransaction.new end
-  end
-
   def create
     @transaction = MaterialTransaction.new(params[:material_transaction])
     @transaction.save
@@ -19,7 +14,6 @@ class MaterialTransactionsController < ApplicationController
     render :nothing => true
   end
 
-  #This is most commonly called from the Edit Observation page
   def destroy
     @transaction = MaterialTransaction.find(params[:id])
     @transaction.destroy
--- app/controllers/setups_controller.rb
+++ app/controllers/setups_controller.rb
@@ -1,10 +1,5 @@
 class SetupsController < ApplicationController
 
-  def new
-    activity = Activity.find_by_id(params[:activity_id])
-    @setup = if activity then activity.setups.new else Setup.new end
-  end
-
   def create
     @setup = Setup.new(params[:setup])
     @setup.save
@@ -19,8 +14,6 @@ class SetupsController < ApplicationController
     render :nothing => true
   end
 
-
-  #This is most commonly called from the Edit Observation page
   def destroy
     @setup = Setup.find(params[:id])
     @setup.destroy
