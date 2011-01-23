782f88bdb83199ab7953860faf104d55eddf5a97
Jonathon Jones <jonathon_d_jones@yahoo.com>
2011-01-23 11:33:23 -0500

    Makes areas_controller_test more rigorous

--- test/functional/areas_controller_test.rb
+++ test/functional/areas_controller_test.rb
@@ -16,6 +16,73 @@ class AreasControllerTest < ActionController::TestCase
       should assign_to :areas
     end
 
+    context 'GET :new' do
+      setup do
+        get :new
+      end
+
+      should ("redirect to the sign in page"){ redirect_to new_person_session_url }
+    end
+
+    context 'POST :create' do
+      setup do
+        assert_nil Area.find_by_name('T2R22')
+        post :create, :area => { :name => 'T2R22' }
+      end
+
+      should ("redirect to the sign in page"){ redirect_to new_person_session_url }
+      should 'create an area' do
+        assert_nil Area.find_by_name('T2R22')
+      end
+    end
+
+    context 'An area exists. ' do
+      setup do
+        @area = Factory.create(:area)
+      end
+
+      context 'GET :show the area' do
+        setup do
+          get :show, :id => @area.id
+        end
+
+        should render_template 'unauthorized_show'
+        should assign_to(:area).with(@area)
+        should assign_to(:observations)
+      end
+
+      context 'GET :edit the area' do
+        setup do
+          get :edit, :id => @area.id
+        end
+
+        should ("redirect to the sign in page"){ redirect_to new_person_session_url }
+      end
+
+      context 'PUT :update the area' do
+        setup do
+          put :update, :id => @area.id, :area => { :name => 'new_area'}
+        end
+
+        should ("redirect to the sign in page"){ redirect_to new_person_session_url }
+        should "not change the area" do
+          @area.reload
+          refute_equal 'new_area', @area.name
+        end
+      end
+
+      context 'DELETE :destroy the area' do
+        setup do
+          delete :destroy, :id => @area.id
+        end
+
+        should ("redirect to the sign in page"){ redirect_to new_person_session_url }
+        should "not destroy the area" do
+          assert Area.find_by_id(@area.id)
+        end
+      end
+    end
+
 
   end
 
@@ -52,6 +119,7 @@ class AreasControllerTest < ActionController::TestCase
       should 'create an area' do
         assert Area.find_by_name('T2R22')
       end
+      should set_the_flash
     end
 
     context "POST :create with xml format" do
@@ -84,6 +152,8 @@ class AreasControllerTest < ActionController::TestCase
         end
 
         should render_template 'authorized_show'
+        should assign_to(:area).with(@area)
+        should assign_to(:observations)
       end
 
       context "GET :edit the area" do
@@ -92,6 +162,7 @@ class AreasControllerTest < ActionController::TestCase
         end
 
         should render_template 'edit'
+        should assign_to(:area).with(@area)
       end
 
       context "PUT :update the area with valid attributes" do
@@ -100,6 +171,10 @@ class AreasControllerTest < ActionController::TestCase
         end
 
         should redirect_to("the area show page") {area_path(@area)}
+        should "change the area" do
+          @area.reload
+          assert_equal 'new_area', @area.name
+        end
       end
 
       context "PUT :update with invalid attributes" do
@@ -109,6 +184,10 @@ class AreasControllerTest < ActionController::TestCase
         end
 
         should render_template 'edit'
+        should "not change the area" do
+          @area.reload
+          refute_equal 'repeat_name', @area.name
+        end
       end
 
       context "DELETE :destroy the area" do
@@ -117,6 +196,9 @@ class AreasControllerTest < ActionController::TestCase
         end
 
         should redirect_to("the areas index") {areas_path}
+        should "destroy the area" do
+          refute Area.find_by_id(@area.id)
+        end
       end
     end
   end
