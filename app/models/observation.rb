b7d9cbad6153c3a4921534accb4abb4b55773aa5
Jonathon Jones <jonathon_d_jones@yahoo.com>
2011-02-13 20:55:51 -0500

    Reorganize observation alphabetically

--- app/models/observation.rb
+++ app/models/observation.rb
@@ -37,16 +37,30 @@ class Observation < ActiveRecord::Base
     where(:state => state)
   end
 
-  def Observation.ordered_by_date
-    order('obs_date desc')
+  def Observation.by_state_and_page(state, page)
+    by_state(state).ordered_by_date.includes_everything.paginate :page => page
   end
 
   def Observation.includes_everything
     includes({:areas => [:study, :treatment]}, :observation_types, {:activities => {:setups => {:material_transactions => :material}}})
   end
 
-  def Observation.by_state_and_page(state, page)
-    by_state(state).ordered_by_date.includes_everything.paginate :page => page
+  def Observation.ordered_by_date
+    order('obs_date desc')
+  end
+
+  def areas_as_text
+    @error_areas || Area.unparse(areas)
+  end
+
+  def areas_as_text=(areas_as_text)
+    @error_areas =  nil
+    new_areas = Area.parse(areas_as_text)
+    if String == new_areas.class
+      @error_areas = new_areas
+    else
+      self.areas  = new_areas
+    end
   end
 
   def equipment_names
@@ -67,8 +81,16 @@ class Observation < ActiveRecord::Base
     end
   end
 
+  def material_names
+    setups.collect { |setup| setup.material_names }.flatten.compact.uniq
+  end
+
   def materials_with_rates
     self.setups.collect { |setup| setup.materials_with_rates }.flatten.uniq.join(', ')
+  end
+
+  def n_contents
+    setups.collect { |setup| setup.n_contents }.flatten.compact.uniq
   end
 
   def observation_type
@@ -77,28 +99,6 @@ class Observation < ActiveRecord::Base
 
   def observation_type_names
     self.observation_types.collect { |type| type.name }.join(', ')
-  end
-
-  def areas_as_text
-    @error_areas || Area.unparse(areas)
-  end
-
-  def areas_as_text=(areas_as_text)
-    @error_areas =  nil
-    new_areas = Area.parse(areas_as_text)
-    if String == new_areas.class
-      @error_areas = new_areas
-    else
-      self.areas  = new_areas
-    end
-  end
-
-  def material_names
-    setups.collect { |setup| setup.material_names }.flatten.compact.uniq
-  end
-
-  def n_contents
-    setups.collect { |setup| setup.n_contents }.flatten.compact.uniq
   end
 
   def rates
