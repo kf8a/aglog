b0d74745f6c767731752b4504e4424d1ae860935
Sven Bohm <bohms@msu.edu>
2011-03-09 08:44:17 -0500

    added removal links and tightend up the placement of the setup partial

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -68,6 +68,17 @@ $(document).ready(function() {
 
 });
 
+function remove_fields(link) {
+  jQuery(link).prev("input[type=hidden]").val("1");
+  jQuery(link).parent().parent(".inputs").first().hide();
+}
+
+function add_fields(link, association, content) {
+  var new_id = new Date().getTime();
+  var regexp = new RegExp("new_" + association, "g")
+  jQuery(link).prev().append(content.replace(regexp, new_id));
+}
+
 var Observation  = {
 	addActivity: function() {
 		index =  Observation.getNewIndex('arrayActivityIndexes');
