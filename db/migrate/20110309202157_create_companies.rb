8ba6f5d49523f670bfabff1ee1abdecfa450a873
Sven Bohm <bohms@msu.edu>
2011-03-14 09:51:34 -0400

    update the old records

--- db/migrate/20110309202157_create_companies.rb
+++ db/migrate/20110309202157_create_companies.rb
@@ -11,6 +11,37 @@ class CreateCompanies < ActiveRecord::Migration
      add_column :people, :company_id, :integer
      add_column :materials, :company_id, :integer
      add_column :areas, :company_id, :integer
+
+     reset_column_information
+    # upgrade old records
+     company = Company.create(:name => 'LTER') 
+     
+     Observations.all.each do |observation|
+       observation.company = company
+       observation.save
+     end
+
+     Equipment.all.each do |e|
+       e.company = company
+       e.save
+     end
+
+     Person.all.each do |p|
+       p.company = company
+       p.save
+     end
+
+     Material.all.each do |m|
+       m.company = company
+       m.save
+     end
+
+     Areas.all.each do |a|
+       a.company = company
+       a.save
+     end
+
+     
   end
 
   def self.down
