efed3b9ac05e32f339769f846a078ab80c8074e5
Sven Bohm <bohms@msu.edu>
2011-03-15 15:02:10 -0400

    simplified study sepc

--- spec/models/study_spec.rb
+++ spec/models/study_spec.rb
@@ -1,11 +1,7 @@
 require 'spec_helper'
 
 describe Study do
-	it "should require unique name" do
-		s = Study.new(:name => 'newname')
-		assert s.save
-		s = Study.new(:name => 'Main')
-		assert !s.save
-	end
+  it {should validate_uniqueness_of :name}
+
 end
 
