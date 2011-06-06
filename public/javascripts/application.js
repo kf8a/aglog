338fa502dd57a30cbe97351b9a1e3164b4fb1019
Sven Bohm <bohms@msu.edu>
2011-06-06 14:29:43 -0400

    basic drag drop, for now the drop only logs to the console

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,10 +1,35 @@
 
 $(document).ready(function() {
     // Put cursor on first text input of form
-    $('.autofocus:first').focus();
-    $('input.ui-date-picker').datepicker();
-    $('#observation_areas_as_text').tokenInput('/areas', {theme:'facebook', preventDuplicates:true} );
+  $('.autofocus:first').focus();
+  $('input.ui-date-picker').datepicker();
+  $('#observation_areas_as_text').tokenInput('/areas', {theme:'facebook', preventDuplicates:true} );
 
+  $('li[draggable=true]').bind('dragstart', function() {
+    this.style.opacity = '0.4';  // this / e.target is the source node.
+  });
+  $('li[draggable=true]').bind('dragend', function() {
+    this.style.opacity = '1';
+  });
+  $('li[draggable=true]').bind('dragenter', function() {
+    $(this).css('border','1px black solid');
+   });
+  $('li[draggable=true]').bind('dragleave', function() {
+    $(this).css('border','none');
+   });
+
+  $('li[draggable=true]').bind('dragover', function(event) {
+    event.stopPropagation();
+    event.preventDefault();
+    return false;
+  });
+
+  $('li[draggable=true]').bind('drop', function(event) {
+    console.log(this);
+    event.stopPropagation();
+    event.preventDefault();
+    return false;
+  });
 
 //    // All links with data_popup make a small popup window of what they link to.
 //    $('.data_popup').live('click', function(e) {
