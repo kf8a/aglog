79c164bc9320baff8bcbd16d350ab34e6b7cc7b7
Jonathon Jones <jonathon_d_jones@yahoo.com>
2011-06-08 16:10:41 -0400

    Simplifies AreaToken by making it subclass String

--- app/models/area_token.rb
+++ app/models/area_token.rb
@@ -1,41 +1,41 @@
-class AreaToken
+class AreaToken < String
 
   def initialize(contents = '', company = 1)
-    @contents = contents
     @company = company.presence || 1
-    self.to_range if @contents.include?('-')
+    super(contents)
   end
 
   def to_range
-    first_part, dash_part, second_part = @contents.partition('-')
+    first_part, dash_part, second_part = self.partition('-')
     first_number_part = second_number_part = ''
     base_part, first_number_part = dissect_part(first_part)
     second_number_part = dissect_part(second_part).last
     first_part  = first_part + first_number_part
     second_part = base_part + second_number_part
 
-    @contents = first_part..second_part
+    first_part..second_part
   end
 
   def dissect_part(part)
     number_part = ''
     until part[-1].to_i == 0
-      number_part = part[-1] + number_part
-      part.chop!
+      number_part = part.slice!(-1) + number_part
     end
 
     [part, number_part]
   end
 
   def number_token?
-    @contents.respond_to?(:to_i) && @contents.to_i != 0
+    self.respond_to?(:to_i) && self.to_i != 0
   end
 
   def to_area
     if number_token?
-      Area.find_by_id(@contents)
+      Area.find_by_id(self)
+    elsif include?('-')
+      Area.find_by_name_and_company_id(self.to_range, @company)
     else
-      Area.find_by_name_and_company_id(@contents, @company)
+      Area.find_by_name_and_company_id(self, @company)
     end
   end
 end
