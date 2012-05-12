06761a8de9dc70637ebf5de8d5532aa2d0389f57
Sven Bohm <bohms@msu.edu>
2012-05-12 11:33:37 -0400

    new version of cucumber-rails

---
+++ lib/html5_builder.rb
@@ -0,0 +1,8 @@
+class HTML5_Builder < Formtastic::SemanticFormBuilder
+  def html5_date(method, otions={})
+    options = set_include_blank(options)
+    html_options = options.delete(:input_html) || {}
+    input_name = generate_association_input_name(method)
+    http://yob.id.au/2010/08/19/extending-formtastic.html
+  end
+end
