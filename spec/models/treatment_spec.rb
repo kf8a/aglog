b55b849656ee95c304b92f27809c24204af7552a
Sven Bohm <bohms@msu.edu>
2014-01-22 19:22:00 -0600

    simplified tests

--- spec/models/treatment_spec.rb
+++ spec/models/treatment_spec.rb
@@ -3,16 +3,17 @@ require 'spec_helper'
 describe Treatment do
   it "should require unique name within a study" do
   	t = Treatment.new(:study_id => 1, :name => 'T11')
-  	assert t.save
+    t.should be_valid
+    assert t.save
   	t = Treatment.new(:study_id => 1, :name => 'T11')
-  	assert !t.save
+    t.should_not be_valid
   end
 
   it "should require valid study" do
   	t = Treatment.new(:study_id => 1)
-  	assert t.save
+    t.should be_valid
   	t = Treatment.new(:study_id => 99)
-  	assert !t.save
+    t.should_not be_valid
 	end
 end
 
