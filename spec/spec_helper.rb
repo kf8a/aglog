4e83418f2e710a7c6dbb3723cc95987a4e116029
Sven Bohm <bohms@msu.edu>
2014-01-22 19:12:56 -0600

    set up for rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,7 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 
-require "#{Rails.root}/db/seeds.rb"
+#require "#{Rails.root}/db/seeds.rb"
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
@@ -42,6 +42,9 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
+
+  config.include FactoryGirl::Syntax::Methods
+
 end
 
 class PersonSessionsController
