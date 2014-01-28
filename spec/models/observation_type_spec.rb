ad3a7f6f1de8ce903aaa09ed86ba39ceb1bcef79
Sven Bohm <bohms@msu.edu>
2014-01-28 13:25:56 -0500

    use local factories

--- spec/models/observation_type_spec.rb
+++ spec/models/observation_type_spec.rb
@@ -4,15 +4,16 @@ describe ObservationType do
   context 'require unique name' do
     before do
       @test_name = 'Harvest'
+      FactoryGirl.create(:observation_type, :name => @test_name)
     end
 
     it "does not allow a duplicate name" do
-      observation  = ObservationType.new(:name => @test_name) # is in fixture already
+      observation  = ObservationType.new(:name => @test_name)
       observation.should_not be_valid
     end
 
     it 'does not allow a duplicate case different name' do
-      observation  = ObservationType.new(:name => @test_name.upcase) # case insensitive
+      observation  = ObservationType.new(:name => @test_name.upcase)
       observation.should_not be_valid
     end
 
