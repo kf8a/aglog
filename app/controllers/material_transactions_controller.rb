d497eff7b1cd756f722a17ee07e0c0b2ccc58cb7
Sven Bohm <bohms@msu.edu>
2014-01-28 12:32:32 -0500

    refactored material_transaction_controller_spec

--- app/controllers/material_transactions_controller.rb
+++ app/controllers/material_transactions_controller.rb
@@ -3,14 +3,14 @@
 class MaterialTransactionsController < ApplicationController
 
   def create
-    @transaction = MaterialTransaction.new(params[:material_transaction])
+    @transaction = MaterialTransaction.new(material_transaction_params)
     @transaction.save
     render :nothing => true
   end
 
   def update
     @transaction = MaterialTransaction.find(params[:id])
-    if @transaction.update_attributes(params[:material_transaction])
+    if @transaction.update_attributes(material_transaction_params)
       flash[:activity] = "Material Transaction was successfully updated."
     end
     render :nothing => true
@@ -20,5 +20,12 @@ class MaterialTransactionsController < ApplicationController
     @transaction = MaterialTransaction.find(params[:id])
     @transaction.destroy
     render :nothing => true
+  end
+
+  private
+  def material_transaction_params
+    params.require(:material_transaction).permit(:material_id, :unit_id, :setup_id,
+                                                 :rate, :cents, :material_transaction_type_id,
+                                                 :transaction_datetime)
   end
 end
