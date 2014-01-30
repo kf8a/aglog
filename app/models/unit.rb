a6ff19ca99f125ee9340206069026b926417f5d6
Sven Bohm <bohms@msu.edu>
2014-01-29 19:28:11 -0600

    start to use si unit names

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -1,9 +1,12 @@
 # Measurements have different units: kilograms, quarts, bushels, etc.
 class Unit < ActiveRecord::Base
-  # attr_accessible :name, :si_unit_id, :conversion_factor
 
   has_many :material_transactions
 
   scope :ordered, -> { order('name')}
   validates :name, :uniqueness => { :case_sensitive => false }
+
+  def si_unit_name
+    Unit.find(si_unit_id).first.name
+  end
 end
