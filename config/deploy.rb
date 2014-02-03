586a495741134da6ba75b95c335c9da68abb7d9d
Sven Bohm <bohms@msu.edu>
2014-02-03 16:22:07 -0500

    added capistrano unicorn

--- config/deploy.rb
+++ config/deploy.rb
@@ -34,20 +34,23 @@ set :linked_files, %w{config/database.yml .env}
 # Default value for keep_releases is 5
 # set :keep_releases, 5
 
+
 namespace :deploy do
 
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
+      invoke 'unicorn:restart'
     end
   end
 
   after :publishing, :restart
 
+
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
+      invoke 'unicorn:reload'
+
       # Here we can do anything such as:
       # within release_path do
       #   execute :rake, 'cache:clear'
