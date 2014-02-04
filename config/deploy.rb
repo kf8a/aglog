c09b55304edb204bd3b0afa3b80e1f44c26522d2
Sven Bohm <bohms@msu.edu>
2014-02-04 02:57:29 -0500

    use the socket again

--- config/deploy.rb
+++ config/deploy.rb
@@ -39,23 +39,14 @@ namespace :deploy do
 
   desc 'Restart application'
   task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
       invoke 'unicorn:restart'
-    end
   end
 
   after :publishing, :restart
 
 
   after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
       invoke 'unicorn:reload'
-
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
   end
 
 end
