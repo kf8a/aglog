0d2d6b597e60b924d237514ce6cd22982649faca
Sven Bohm <bohms@msu.edu>
2014-04-16 18:33:13 -0500

    scaffolding for uploading equipment pictures

---
+++ db/migrate/20140416232736_create_equipment_pictures.rb
@@ -0,0 +1,10 @@
+class CreateEquipmentPictures < ActiveRecord::Migration
+  def change
+    create_table :equipment_pictures do |t|
+      t.integer :equipment_id
+      t.string :title
+      t.text   :description
+      t.timestamps
+    end
+  end
+end
