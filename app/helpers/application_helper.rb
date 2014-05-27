7a31e2d5c478753066d9e9138400b0185babddbd
Sven Bohm <bohms@msu.edu>
2014-05-27 14:22:03 -0400

    basic remove fields seems to work

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
 
-  def link_to_remove_fields(name, f)
-    f.hidden_field(:_destroy) + link_to(name, "remove_fields(this)")
-  end
+  # def link_to_remove_fields(name, f)
+  #   f.hidden_field(:_destroy) + link_to(name, "remove_fields(this)")
+  # end
 
   def link_to_add_fields(name, f, association, only_current)
     new_object = f.object.send(association).klass.new
