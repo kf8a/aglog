067a5a270cf18d21d58d942e5b10a22fdc5a21dc
Sven Bohm <bohms@msu.edu>
2015-02-04 12:27:07 -0500

    added space for multi file uploader

---
+++ app/uploaders/notes_uploader.rb
@@ -0,0 +1,27 @@
+# encoding: utf-8
+
+class NotesUploader < CarrierWave::Uploader::Base
+
+  storage :file
+  # storage :fog
+
+  # Override the directory where uploaded files will be stored.
+  # This is a sensible default for uploaders that are meant to be mounted:
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  # Provide a default URL as a default if there hasn't been a file uploaded:
+  # def default_url
+  #   # For Rails 3.1+ asset pipeline compatibility:
+  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+  #
+  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
+  # end
+
+  # Add a white list of extensions which are allowed to be uploaded.
+  # For images you might use something like this:
+  def extension_white_list
+    %w(jpg jpeg gif png pdf xls xlsx)
+  end
+end
