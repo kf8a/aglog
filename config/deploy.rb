0900b31c9cfca16d0bc8f3ca5c858b1649317a60
Sven Bohm <bohms@msu.edu>
2015-02-04 16:47:45 -0500

    make sure the public uploads file gets linked in

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for Capistrano 3.1
-lock '3.2.1'
+lock '3.3.5'
 
 set :application, 'aglog'
 set :repo_url, 'https://github.com/kf8a/aglog.git'
@@ -26,7 +26,7 @@ set :scm, :git
 set :linked_files, %w{config/database.yml .env}
 
 # Default value for linked_dirs is []
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+ set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
