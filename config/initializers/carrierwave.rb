ee2a3fee0053260bab3163dfb16ea1d9db1844e9
Sven Bohm <bohms@msu.edu>
2015-02-05 15:40:57 -0500

    scaffold to help test the upload

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,3 +2,9 @@ CarrierWave.configure do |config|
     config.remove_previously_stored_files_after_update = false
 end
 
+if Rails.env.test? or Rails.env.cucumber?
+  CarrierWave.configure do |config|
+    config.storage = :file
+    config.enable_processing = false
+  end
+end
