6e96a2534df9859a8da1d3f9babc82d9fc657948
Sven Bohm <bohms@msu.edu>
2015-02-23 14:02:20 -0500

    silence deprecation warnings

--- spec/controllers/material_transactions_controller_spec.rb
+++ spec/controllers/material_transactions_controller_spec.rb
@@ -7,10 +7,10 @@ describe MaterialTransactionsController, type: :controller  do
   let(:material)             { FactoryGirl.build_stubbed(:material) }
 
   before :each do
-    material_transaction.stub(:save).and_return(true)
-    MaterialTransaction.stub(:persisted?).and_return(true)
-    MaterialTransaction.stub(:find).with(material_transaction.id.to_s).and_return(material_transaction)
-    MaterialTransaction.stub(:by_company).and_return(MaterialTransaction)
+    allow(material_transaction).to receive(:save).and_return(true)
+    allow(MaterialTransaction).to receive(:persisted?).and_return(true)
+    allow(MaterialTransaction).to receive(:find).with(material_transaction.id.to_s).and_return(material_transaction)
+    allow(MaterialTransaction).to receive(:by_company).and_return(MaterialTransaction)
   end
 
   describe "Not signed in. " do
@@ -42,7 +42,7 @@ describe MaterialTransactionsController, type: :controller  do
       end
 
       it "should create a material transaction" do
-        expect(MaterialTransaction.exists?(assigns[:transaction])).to eq true
+        expect(MaterialTransaction.exists?(assigns[:transaction].id)).to eq true
       end
     end
 
@@ -53,7 +53,7 @@ describe MaterialTransactionsController, type: :controller  do
         end
 
         it 'creates a new material_transaction' do
-          expect(MaterialTransaction.exists?(assigns[:transaction])).to eq true
+          expect(MaterialTransaction.exists?(assigns[:transaction].id)).to eq true
         end
       end
 
@@ -63,7 +63,7 @@ describe MaterialTransactionsController, type: :controller  do
         end
 
         it 'creates a new material_transaction' do
-          expect(MaterialTransaction.exists?(assigns[:transaction])).to eq false
+          expect(MaterialTransaction.exists?(assigns[:transaction].id)).to eq false
         end
       end
     end
@@ -72,14 +72,14 @@ describe MaterialTransactionsController, type: :controller  do
 
       context 'valid attributes' do
         it "locates the reqested material_transaction" do
-          material_transaction.should_receive(:update_attributes).with({"setup_id" => "1"}).and_return(true)
+          expect(material_transaction).to receive(:update_attributes).with({"setup_id" => "1"}).and_return(true)
           put :update, :id => material_transaction, :material_transaction => { :setup_id => 1}
           expect(assigns[:transaction]).to eq material_transaction
         end
       end
       context 'invalid attributes' do
         before(:each) do
-          material_transaction.should_receive(:update_attributes).and_return(false)
+          expect(material_transaction).to receive(:update_attributes).and_return(false)
           put :update, :id => material_transaction, :material_transaction => { :setup_id => 1}
         end
         it 'should locate the requested material_transaction' do
@@ -94,12 +94,12 @@ describe MaterialTransactionsController, type: :controller  do
 
     describe "DELETE :destroy a material transaction" do
       before(:each) do
-        material_transaction.stub(:destroy).and_return(true)
+        allow(material_transaction).to receive(:destroy).and_return(true)
         delete :destroy, :id => material_transaction
       end
 
       it "should delete the transaction" do
-        expect(MaterialTransaction.exists?(material_transaction)).to eq false
+        expect(MaterialTransaction.exists?(material_transaction.id)).to eq false
       end
     end
   end
