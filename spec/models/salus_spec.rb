d2db78c4560cfa4732baa52329f8407f057cc4a4
Sven Bohm <bohms@msu.edu>
2015-04-10 15:51:20 -0400

    describe what the salus export should look like

--- spec/models/salus_spec.rb
+++ spec/models/salus_spec.rb
@@ -3,20 +3,34 @@ require 'rails_helper'
 RSpec.describe Salus, :type => :model do
   before(:each) do
     @area = find_or_factory(:area)
-    # observation = FactoryGirl.create :observation, {observation_types: []}
-    # @planting = @area.observation << observation
     @salus = Salus.new
     @salus.area = @area
   end
+
+  it 'returns a list of operations on the field in chronological order' do
+    # it starts out in an undefined state and then proceeds from tillage to planting to harvest
+  end
+
   it "returns a list of planting dates" do
-    expect(@salus.planting_records).to eq []
+    # observation_type = ObservationType.where(name: "Planting").first
+    # material_type = FactoryGirl.create :material_type, name: "seed"
+    # material = FactoryGirl.create :material, name: "corn", material_type_id: material_type.id
+    # observation = FactoryGirl.create :observation, {observation_types: [observation_type]}
+    # @area.observations << observation
+    # expect(@salus.planting_records).to eq [observation]
   end
 
   it "returns a list of harvest dates" do
-    expect(@salus.harvest_records).to eq []
+    observation_type = ObservationType.where(name: "Harvest").first
+    observation = FactoryGirl.create :observation, {observation_types: [observation_type]}
+    @area.observations << observation
+    expect(@salus.harvest_records).to eq [observation]
   end
 
   it "returns a list of tillage events" do
-    expect(@salus.tillage_records).to eq []
+    observation_type = ObservationType.where(name: "Soil Preparation").first
+    observation = FactoryGirl.create :observation, {observation_types: [observation_type]}
+    @area.observations << observation
+    expect(@salus.tillage_records).to eq [observation]
   end
 end
