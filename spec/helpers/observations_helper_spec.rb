06b49f494ca75b6be248fb065aca1612b79c70ee
Sven Bohm <bohms@msu.edu>
2015-04-13 21:50:08 -0400

    silence deprecation warnings

--- spec/helpers/observations_helper_spec.rb
+++ spec/helpers/observations_helper_spec.rb
@@ -4,24 +4,24 @@ describe ObservationsHelper, type: :helper do
   describe "#initialize_arrays" do
     it "should give the right results" do
       js_stuff = "<script>\n//<![CDATA[\narrayActivityIndexes = []; arraySetupIndexes=[];\n//]]>\n</script>"
-      helper.initialize_arrays.should eq(js_stuff)
+      expect(helper.initialize_arrays).to eq(js_stuff)
     end
   end
 
   describe "#observation_type_ids" do
     it "should give an array of observation types" do
       correct_ones = [["Soil Preparation", 1], ["Harvest", 2], ["Planting", 3], ["Fertilizer application", 4], ["Pesticide application", 5], ["Herbicide application", 6]]
-      helper.observation_type_ids.should eq(correct_ones)
+      expect(helper.observation_type_ids).to eq(correct_ones)
     end
   end
 
   describe "#format_cell" do
     it "should return '_' if given an empty array" do
-      helper.format_cell([]).should eq('_')
+      expect(helper.format_cell([])).to eq('_')
     end
 
     it "should return the elements formatted for csv" do
-      helper.format_cell(['bird', 'plane', 'superman']).should eq('bird;;plane;;superman')
+      expect(helper.format_cell(['bird', 'plane', 'superman'])).to eq('bird;;plane;;superman')
     end
   end
 end
