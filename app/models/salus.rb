bd31f2a2510af6d83974764cdc7745a75ee7d0bf
Sven Bohm <bohms@msu.edu>
2015-04-20 15:55:14 -0400

    added harvest component to salus

--- app/models/salus.rb
+++ app/models/salus.rb
@@ -1,6 +1,25 @@
 class Salus
   attr_accessor :area
 
+  def rotation_components_for(year)
+    planting_component_for(year)
+    fertilizer_component_for(year)
+    harvest_component_for(year)
+  end
+
+  def planting_component_for(year)
+  end
+
+  def fertilizer_component_for(year)
+  end
+
+  def harvest_component_for(year)
+    results = harvest_records.where("date_part('year', obs_date) = ?",year)
+    results.collect do |result|
+      "<Mgt_Harvest_App Year='#{result.obs_date.year}' DOY='#{result.obs_date.yday}' HCom='H' HSiz='A' HPc='100' HBmin='0' HBPc='0' HKnDnPc='0' />"
+    end.join("\n")
+  end
+
   def planting_records
     area.observations.joins(:observation_types, setups: [:materials])
       .where("material_type_id = ?", MaterialType.where(name: 'seed').first)
