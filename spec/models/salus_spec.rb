23af6831ee722467b0fcfd110e35c2bf0409ec2a
Sven Bohm <bohms@msu.edu>
2015-04-20 15:55:14 -0400

    added harvest component to salus

--- spec/models/salus_spec.rb
+++ spec/models/salus_spec.rb
@@ -7,6 +7,21 @@ RSpec.describe Salus, :type => :model do
     @salus.area = @area
   end
 
+  it 'returns planting components for the year' do
+  end
+
+  it 'returns harvest components for the year' do
+    observation_type = ObservationType.where(name: "Harvest").first
+    observation = FactoryGirl.create :observation, {observation_types: [observation_type]}
+    @area.observations << observation
+    result = "<Mgt_Harvest_App Year='#{Date.today.year}' DOY='#{Date.today.yday}' HCom='H' HSiz='A' HPc='100' HBmin='0' HBPc='0' HKnDnPc='0' />"
+    expect(@salus.harvest_component_for(Date.today.year)).to eq result
+  end
+
+  it 'returns a rotation component for the year' do
+   expect(@salus.rotation_components_for(2015)).to_not be_nil
+  end
+
   it 'returns a list of operations on the field in chronological order' do
     # it starts out in an undefined state and then proceeds from tillage to planting to harvest
   end
