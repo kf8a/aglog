40108eaa96137b8117c79d52d792b6f337a05274
Sven Bohm <bohms@msu.edu>
2015-04-24 21:23:58 -0400

    removed old code

--- app/models/salus.rb
+++ app/models/salus.rb
@@ -9,7 +9,6 @@ class Salus
   end
 
   def crop_for(year)
-    records = planting_records_for(year)
     #TODO this is not true in general
     # records.first.activities.first.setups.first.materials.first.name
     "corn"
@@ -36,7 +35,7 @@ class Salus
       end
       current_type = obs.observation_type
     end
-    p result
+    # p result
     result
   end
 
@@ -76,75 +75,10 @@ class Salus
     tillage_records + harvest_records + planting_records + fertilizer_records
   end
 
-  def rotation_components_for(year)
-    [planting_components_for(year), 
-      fertilizer_components_for(year),
-      tillage_components_for(year), 
-      harvest_components_for(year)].compact.join("\n")
-  end
-
-  def planting_component_xml(obs, activity, setup, transaction)
-            "<Mgt_Planting CropMod='S' SpeciesID='#{transaction.material.salus_code}' CultivarID='IB1003' Year='#{obs.obs_date.year}' DOY='#{obs.obs_date.yday}' EYear='0' EDOY='' Ppop='#{transaction.seeds_per_square_meter}' Ppoe='#{transaction.seeds_per_square_meter}' PlMe='S' PlDs='R' RowSpc='10' AziR='' SDepth='4' SdWtPl='20' SdAge='' ATemp='' PlPH='' src='#{url_for(obs)}' notes='#{obs.comment}' />"
-  end
-
-  #TODO where should we keep row spacing seed weight and planting depth
-  def planting_components_for(year)
-    results = planting_records_for(year)
-    results.flat_map do |result|
-      result.activities.flat_map do |activity|
-        activity.setups.flat_map do |setup|
-          setup.material_transactions.flat_map do |transaction|
-            planting_component_xml(result, activity, setup, transaction)
-          end
-        end
-      end
-    end.join("\n")
-  end
-
-  def fertilizer_components_for(year)
-    results = fertilizer_records_for(year)
-    results.flat_map do |result|
-      result.activities.flat_map do |activity|
-        activity.setups.flat_map do |setup|
-          setup.material_transactions.flat_map do |transaction|
-            #TODO add n p k ca content
-            "<Mgt_Fertilizer_App Year ='#{result.obs_date.year} DOY='#{result.obs_date.yday}' AKFer='' ANFer='#{}' APFer='' src='#{url_for(result)}' notes='#{result.comment}'/>"
-          end
-        end
-      end
-    end.join("\n")
-  end
-
-  def tillage_components_for(year)
-    results = tillage_records_for(year)
-    results.flat_map do |result|
-      result.activities.flat_map do |activity|
-        activity.setups.flat_map do |setup|
-          next if setup.equipment.is_tractor?
-          "<Mgt_Tillage_App Year='#{result.obs_date.year}' DOY='#{result.obs_date.yday}' TDep='6' TImpl='#{setup.equipment.salus_code}' src='#{url_for(result)}' notes='#{result.comment}'/>"
-        end
-      end
-    end.join("\n")
-  end
-
-  def harvest_components_for(year)
-    results = harvest_records_for(year)
-    results.flat_map do |result|
-    "<Mgt_Harvest_App Year='#{result.obs_date.year}' DOY='#{result.obs_date.yday}' HCom='H' HSiz='A' HPc='100' HBmin='0' HBPc='0' HKnDnPc='0' src='#{url_for(result)}' notes='#{result.comment}'/>"
-    end.join("\n")
-  end
-
   def fertilizer_records
     area.observations.joins(:observation_types, setups: [:material_transactions, {materials: :material_type} ])
       .where("material_types.name = ?", "fertilizer")
       .where("observation_types.name = ?","Fertilizer application")
-  end
-
-  def fertilizer_records_for(year)
-    area.observations.joins(:observation_types, setups: [:material_transactions, {materials: :material_type} ])
-      .where("material_types.name = ?", "fertilizer")
-      .where("observation_types.name = ?","Fertilizer application")
-      .where("date_part('year', obs_date) =?", year)
   end
 
   def planting_records
@@ -153,21 +87,8 @@ class Salus
       .where("observation_types.name = ?","Planting")
   end
 
-  def planting_records_for(year)
-    area.observations.joins(:observation_types, setups: [:material_transactions, {materials: :material_type} ])
-      .where("material_types.name = ?", "seed")
-      .where("observation_types.name = ?","Planting")
-      .where("date_part('year', obs_date) =?", year)
-  end
-
   def harvest_records
     area.observations.joins(:observation_types)
-      .where("observation_types.name = ?", 'Harvest')
-  end
-
-  def harvest_records_for(year)
-    area.observations.joins(:observation_types)
-      .where("date_part('year', obs_date) =?", year)
       .where("observation_types.name = ?", 'Harvest')
   end
 
@@ -175,13 +96,6 @@ class Salus
     area.observations.joins(:observation_types)
       .where("observation_types.name = ?", "Soil Preparation")
   end
-
-  def tillage_records_for(year)
-    area.observations.joins(:observation_types)
-      .where("date_part('year', obs_date) =?", year)
-      .where("observation_types.name = ?", "Soil Preparation")
-  end
-
 
   def url_for(object)
     "https://aglog.kbs.msu.edu/observations/#{object.id}"
