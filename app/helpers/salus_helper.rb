a88a51de6586a763af91d8efba4536c5821d0b26
Sven Bohm <bohms@msu.edu>
2015-04-28 11:14:47 -0400

    fixed start date and added newlines for human readability

--- app/helpers/salus_helper.rb
+++ app/helpers/salus_helper.rb
@@ -3,13 +3,13 @@ module SalusHelper
     logger.info "current #{component}"
     case component[:type]
     when 'harvest'
-        "<Mgt_Harvest_App Year='#{component[:year]}' DOY='#{component[:doy]}' DAP='' HStg='' HCom='H' HSiz='A' HPc='100' HBmin='0' HBPc='0' HKnDnPc='0' url='#{component[:url]}' notes='#{component[:notes]}'/ >"
+        "<Mgt_Harvest_App Year='#{component[:year]}' DOY='#{component[:doy]}' DAP='' HStg='' HCom='H' HSiz='A' HPc='100' HBmin='0' HBPc='0' HKnDnPc='0' url='#{component[:url]}' notes='#{component[:notes]}'/ >\n"
     when 'planting'
-        "<Mgt_Planting CropMod='S' SpeciesID='#{component[:species]}' CultivarID='' Year='#{component[:year]}' DOY='#{component[:doy]}' EYear='0' EDOY='' Ppop='#{component[:ppop]}' Ppoe='' PlMe='S' PlDs='' RowSpc='' AziR='' SDepth='' SdWtPl='' SdAge='' ATemp='' PlPH='' url='#{component[:url]}' notes='#{component[:notes]}' />"
+        "<Mgt_Planting CropMod='S' SpeciesID='#{component[:species]}' CultivarID='' Year='#{component[:year]}' DOY='#{component[:doy]}' EYear='0' EDOY='' Ppop='#{component[:ppop]}' Ppoe='' PlMe='S' PlDs='' RowSpc='' AziR='' SDepth='' SdWtPl='' SdAge='' ATemp='' PlPH='' url='#{component[:url]}' notes='#{component[:notes]}' />\n"
     when 'tillage'
-      "<Mgt_Tillage_App Year='#{component[:year]}' DOY='#{component[:doy]}' url='#{component[:url]}' notes='#{component[:notes]}'/>"
+      "<Mgt_Tillage_App Year='#{component[:year]}' DOY='#{component[:doy]}' url='#{component[:url]}' notes='#{component[:notes]}'/>\n"
     when 'fertilizer'
-        "<Mgt_Fertilizer_App DSoilN='10' SoilNC='95' SoilNX='50' NCode='#{component[:fertilizer]}' NEnd='4' url='#{component[:url]}' notes='#{component[:notes]}'/>"
+        "<Mgt_Fertilizer_App DSoilN='10' SoilNC='95' SoilNX='50' NCode='#{component[:fertilizer]}' NEnd='4' url='#{component[:url]}' notes='#{component[:notes]}'/>\n"
     when 'irrigation'
   
     end
