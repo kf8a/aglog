5be42f95caf0cfaf50f37e64bd397388761c2cc5
Sven Bohm <bohms@msu.edu>
2015-04-28 13:02:05 -0400

    use html_safe to prevent escaping of the xml tags

--- app/helpers/salus_helper.rb
+++ app/helpers/salus_helper.rb
@@ -3,13 +3,23 @@ module SalusHelper
     logger.info "current #{component}"
     case component[:type]
     when 'harvest'
-        "<Mgt_Harvest_App Year='#{component[:year]}' DOY='#{component[:doy]}' DAP='' HStg='' HCom='H' HSiz='A' HPc='100' HBmin='0' HBPc='0' HKnDnPc='0' url='#{component[:url]}' notes='#{component[:notes]}'/ >\n"
+        result = "<Mgt_Harvest_App Year='#{component[:year]}' DOY='#{component[:doy]}' DAP='' HStg='' HCom='H' HSiz='A' HPc='100' HBmin='0' HKnDnPc='0' url='#{component[:url]}' notes='".html_safe 
+        result <<  component[:notes]
+        result << "'/>".html_safe
+        result
     when 'planting'
-        "<Mgt_Planting CropMod='S' SpeciesID='#{component[:species]}' CultivarID='' Year='#{component[:year]}' DOY='#{component[:doy]}' EYear='0' EDOY='' Ppop='#{component[:ppop]}' Ppoe='' PlMe='S' PlDs='' RowSpc='' AziR='' SDepth='' SdWtPl='' SdAge='' ATemp='' PlPH='' url='#{component[:url]}' notes='#{component[:notes]}' />\n"
+        result = "<Mgt_Planting CropMod='S' SpeciesID='#{component[:species]}' CultivarID='' Year='#{component[:year]}' DOY='#{component[:doy]}' EYear='0' EDOY='' Ppop='#{component[:ppop]}' Ppoe='' PlMe='S' PlDs='' RowSpc='' AziR='' SDepth='' SdWtPl='' SdAge='' ATemp='' PlPH='' url='#{component[:url]}' notes='".html_safe
+       result <<  component[:notes] 
+       result <<  "' />".html_safe
+       result
     when 'tillage'
-      "<Mgt_Tillage_App Year='#{component[:year]}' DOY='#{component[:doy]}' url='#{component[:url]}' notes='#{component[:notes]}'/>\n"
+      result = "<Mgt_Tillage_App Year='#{component[:year]}' DOY='#{component[:doy]}' url='#{component[:url]}' notes='".html_safe 
+      result << component[:notes]
+      result << "'/>".html_safe
     when 'fertilizer'
-        "<Mgt_Fertilizer_App DSoilN='10' SoilNC='95' SoilNX='50' NCode='#{component[:fertilizer]}' NEnd='4' url='#{component[:url]}' notes='#{component[:notes]}'/>\n"
+        result = "<Mgt_Fertilizer_App DSoilN='10' SoilNC='95' SoilNX='50' NCode='#{component[:fertilizer]}' NEnd='4' url='#{component[:url]}' notes='".html_safe
+        result << component[:notes]
+        result << "'/>".html_safe
     when 'irrigation'
   
     end
