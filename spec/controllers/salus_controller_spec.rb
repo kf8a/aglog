48eedfc3f0071669bff2a410011601ac7071a32e
Sven Bohm <bohms@msu.edu>
2015-04-28 09:58:22 -0400

    add the helper to return xml rotation components

---
+++ spec/controllers/salus_controller_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe SalusController, type: :controller do
+
+  describe "GET #show" do
+    it "returns http success" do
+      @area = FactoryGirl.create :area
+      get :show, id: @area.id, format: :xml
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
