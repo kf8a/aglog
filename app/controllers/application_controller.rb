6feb3df8bd29c1d1bb0c2fb1851768a1bc557c79
Sven Bohm <bohms@msu.edu>
2016-01-05 14:13:05 -0500

    update the protect_from_forgery line for rails 4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 # Filters added to this controller will be run for all controllers in the application.
 # Likewise, all the methods added will be available for all controllers.
 class ApplicationController < ActionController::Base
-  protect_from_forgery
+   protect_from_forgery :with => :exception
   respond_to :html, :xml
 
   before_filter :authenticate_user!, :except => [:index, :show] # unless Rails.env() == 'development'
