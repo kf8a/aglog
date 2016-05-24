cd0280c8a700ced6249b1151d27991b1b4b0a9c9
Sven Bohm <bohms@msu.edu>
2016-05-24 08:20:48 -0400

    style fixes

--- app/helpers/material_transactions_helper.rb
+++ app/helpers/material_transactions_helper.rb
@@ -1,11 +1,13 @@
+# helpers for material view
 module MaterialTransactionsHelper
   def materials
-    @materials ||= Material.order('material_type_id, name').collect do |material|
+    @materials ||= Material.order('material_type_id, name')
+                           .collect do |material|
       [material.name, material.id]
     end
   end
 
   def units
-    @units ||= Unit.order('name').collect {|unit| [unit.name, unit.id]}
+    @units ||= Unit.order('name').collect { |unit| [unit.name, unit.id] }
   end
 end
--- app/models/equipment_picture.rb
+++ app/models/equipment_picture.rb
@@ -1,3 +1,5 @@
+# Equipment types classify equipment
+# for example Planter, Spreader, Tractor
 class EquipmentPicture < ActiveRecord::Base
   belongs_to :equipment
   mount_uploader :equipment_picture, EquipmentPictureUploader
