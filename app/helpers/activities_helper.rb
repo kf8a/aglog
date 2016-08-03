4cb19ad7442f8120fe9efe2b4300f1ac71df6b9f
Sven Bohm <bohms@msu.edu>
2016-08-03 15:56:17 -0400

    style fixes

--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -1,7 +1,8 @@
+# helper methods for activity views
 module ActivitiesHelper
-   def people
-     @people ||= Person.order('sur_name, given_name').collect do |person|
-       [person.name, person.id]
-     end
-   end
+  def people
+    @people ||= Person.order('sur_name, given_name').collect do |person|
+      [person.name, person.id]
+    end
+  end
 end
