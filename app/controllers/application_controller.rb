4d57e2f1b3150bf34968d520bcf14d5a962ab470
Sven Bohm <bohms@msu.edu>
2016-08-03 15:56:17 -0400

    style fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
-# Filters added to this controller will be run for all controllers in the application.
+# Filters added to this controller will be run for all controllers
+# in the application.
 # Likewise, all the methods added will be available for all controllers.
 class ApplicationController < ActionController::Base
-   protect_from_forgery :with => :exception
+  protect_from_forgery with: :exception
   respond_to :html, :xml
 
-  before_filter :authenticate_user!, :except => [:index, :show] # unless Rails.env() == 'development'
+  before_filter :authenticate_user!, except: [:index, :show]
 end
