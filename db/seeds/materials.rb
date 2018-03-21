de2782cade4ebf042d6c0a19d463cba4d901e64a
Sven Bohm <bohms@msu.edu>
2018-03-20 20:53:12 -0400

    style fixes

--- db/seeds/materials.rb
+++ db/seeds/materials.rb
@@ -1,9 +1,11 @@
-Material.find_or_create_by(:name => 'nothing')
-Material.find_or_create_by(:name => 'seed corn', :operation_type_id => 3)
-Material.find_or_create_by(:name => 'seed wheat', :operation_type_id => 3)
-Material.find_or_create_by(:name => '60-0-0', :operation_type_id => 4)
-Material.find_or_create_by(:name => 'corn', :operation_type_id => 5)
-Material.find_or_create_by(:name => 'wheat', :operation_type_id => 5)
+# frozen_string_literal: true
+
+Material.find_or_create_by(name: 'nothing')
+Material.find_or_create_by(name: 'seed corn', operation_type_id: 3)
+Material.find_or_create_by(name: 'seed wheat', operation_type_id: 3)
+Material.find_or_create_by(name: '60-0-0', operation_type_id: 4)
+Material.find_or_create_by(name: 'corn', operation_type_id: 5)
+Material.find_or_create_by(name: 'wheat', operation_type_id: 5)
 
 Material.all.each do |m|
   m.specific_weight = 1
--- db/seeds/observation_types.rb
+++ db/seeds/observation_types.rb
@@ -1,6 +1,8 @@
-ObservationType.find_or_create_by(:name => 'Soil Preparation')
-ObservationType.find_or_create_by(:name => 'Harvest')
-ObservationType.find_or_create_by(:name => 'Planting')
-ObservationType.find_or_create_by(:name => 'Fertilizer application')
-ObservationType.find_or_create_by(:name => 'Pesticide application')
-ObservationType.find_or_create_by(:name => 'Herbicide application')
+# frozen_string_literal: true
+
+ObservationType.find_or_create_by(name: 'Soil Preparation')
+ObservationType.find_or_create_by(name: 'Harvest')
+ObservationType.find_or_create_by(name: 'Planting')
+ObservationType.find_or_create_by(name: 'Fertilizer application')
+ObservationType.find_or_create_by(name: 'Pesticide application')
+ObservationType.find_or_create_by(name: 'Herbicide application')
