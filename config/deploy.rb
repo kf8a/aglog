0f681923e2f2c12c866c4c234966825fc6bd0c49
Sven Bohm <bohms@msu.edu>
2018-05-12 09:42:40 -0400

    fix deprecation warning for capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,17 +1,12 @@
 # config valid only for Capistrano 3.1
-lock '3.6.1'
+lock '3.10.2'
 
 set :application, 'aglog'
 set :repo_url, 'https://github.com/kf8a/aglog.git'
 
-# Default branch is :master
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
 # Default deploy_to directory is /var/www/my_app
 set :deploy_to, '/var/u/apps/aglog'
-
-# Default value for :scm is :git
-set :scm, :git
 
 # Default value for :format is :pretty
 # set :format, :pretty
