3f907a13c8bef04eacdb62dcb78598d42c489a50
Sven Bohm <bohms@msu.edu>
2018-05-24 11:52:02 -0400

    move the doing out of the controller and let the database do the filtering

--- app/controllers/pesticides_controller.rb
+++ app/controllers/pesticides_controller.rb
@@ -3,14 +3,7 @@
 # Returns the list of fields that have had pesticides applied
 class PesticidesController < ApplicationController
   def index
-    @closed_fields = ObservationType.find(5)
-                                    .observations.where(company_id: 1)
-                                    .order('obs_date')
-                                    .collect do |record|
-      reentry_date = record.obs_date + 30.days
-      next unless reentry_date > Date.today
-      { date: reentry_date, areas: record.areas_as_text }
-    end.compact
+    @closed_fields = Pesticide.closed_fields
 
     render :index
   end
