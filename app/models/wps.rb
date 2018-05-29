ce08e9e1f8a6b3ec74887f1e7e0dba21195d7af5
Sven Bohm <bohms@msu.edu>
2018-05-29 13:29:23 -0400

    mvoe to wps url

--- app/models/wps.rb
+++ app/models/wps.rb
@@ -1,15 +1,21 @@
 # frozen_string_literal: true
 
-# Retrieve a list of closed fields
-class Pesticide
+# Worker Protection Standard helpers
+class Wps
   def self.closed_fields
     ObservationType.find(5)
                    .observations.where(company_id: 1)
                    .where('obs_date > ?', Date.today - 30.days)
                    .order('obs_date')
                    .collect do |record|
-      reentry_date = record.obs_date + 30.days
-      { date: reentry_date, areas: record.areas_as_text }
+      records(record)
+    end.flatten
+  end
+
+  def self.records(record)
+    reentry_date = record.obs_date + 30.days
+    Area.unparse(record.areas).split(/ /).map do |area|
+      { date: reentry_date, area: area }
     end
   end
 end
