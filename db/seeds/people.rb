7d6086d9ec59d18a327129c0e94efe871667972b
Sven Bohm <bohms@msu.edu>
2018-06-27 11:15:49 -0400

    set up required default membership

--- db/seeds/people.rb
+++ db/seeds/people.rb
@@ -1,1 +1,3 @@
-Person.find_or_create_by(:given_name => 'Joe', :sur_name => 'Simmons')
+person = Person.find_or_create_by(:given_name => 'Joe', :sur_name => 'Simmons')
+company = Company.first
+Membership.create(person_id: person.id, company_id: company.id, default_company: true)
