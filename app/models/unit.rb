ce81f18e0f6212719750d142ecf76c6fe6da9a6f
Sven Bohm <bohms@msu.edu>
2018-08-20 11:01:31 -0400

    return nil if we do not have an si unit

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -6,7 +6,7 @@ class Unit < ActiveRecord::Base
   validates :name, uniqueness: { case_sensitive: false }
 
   def si_unit_name
-    return if si_unit_id
+    return unless si_unit_id
     Unit.find(si_unit_id).name
   end
 
