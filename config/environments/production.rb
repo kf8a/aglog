85d3b5de85be17cca0219259a48de382c6a049e5
sven <bohms@ideomatic.io>
2019-11-01 15:31:41 -0400

    add log tags

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,7 @@ Aglog::Application.configure do
   config.log_level = :info
 
   # Prepend all log lines with the following tags
-  # config.log_tags = [ :subdomain, :uuid ]
+  config.log_tags = [ :subdomain, :uuid ]
 
   # Use a different logger for distributed setups
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
