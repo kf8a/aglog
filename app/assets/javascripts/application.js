efa34eac877bffaf8d1959eb101f2730f884cfd0
sven <bohms@ideomatic.io>
2019-11-21 13:00:18 -0500

    updates jquery and added datatables to equipment table

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,5 @@
-//= require jquery
-//= require jquery_ujs
-//= require jquery-ui
 //= require jquery.tokeninput
 //= require form_functions
 //= require tree
 //= require_tree .
+//= require sortable_table
