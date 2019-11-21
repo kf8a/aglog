9ea3b4c5d6585418f7eec45acb384e1f373cffac
sven <bohms@ideomatic.io>
2019-11-21 13:29:45 -0500

    fix bootstrap 4 styles

--- app/assets/javascripts/sortable_table.js
+++ app/assets/javascripts/sortable_table.js
@@ -2,15 +2,6 @@ jQuery(document).ready(function() {
   $('.sortable_table').DataTable(
     {
       paging: false,
-      "columns": [
-        null,
-        null,
-        null,
-        null,
-        { "orderable": false },
-        { "orderable": false },
-        { "orderable": false },
-      ]
     }
   );
 } );
