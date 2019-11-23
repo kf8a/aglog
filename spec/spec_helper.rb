07004d5e73f0acd9e6b131526a5b0aec45177417
sven <bohms@ideomatic.io>
2019-11-23 11:19:54 -0500

    remove database cleaner

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,6 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
-require 'database_cleaner'
 
 # require "#{Rails.root}/db/seeds.rb"
 
@@ -38,24 +37,7 @@ RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
-  config.mock_with :rspec do |mocks|
-    # Prevents you from mocking or stubbing a method that does not exist on
-    # a real object. This is generally recommended, and will default to
-    # `true` in RSpec 4.
-    # mocks.verify_partial_doubles = true
-  end
   config.order = 'random'
-
-  config.before :each do
-    DatabaseCleaner.strategy = :transaction
-    DatabaseCleaner.start
-  end
-
-  config.after(:each) do
-    DatabaseCleaner.clean
-  end
-
-  # config.include FactoryBot::Syntax::Methods
 end
 
 Shoulda::Matchers.configure do |config|
