64f9b58e45eee5c9611bb11296b77e0e0950d7f5
sven <bohms@ideomatic.io>
2019-11-25 20:06:27 -0500

    manually load the webpack helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@
 # in the application.
 # Likewise, all the methods added will be available for all controllers.
 class ApplicationController < ActionController::Base
+  helper Webpacker::Helper
+
   protect_from_forgery with: :exception
   respond_to :html, :xml
 
