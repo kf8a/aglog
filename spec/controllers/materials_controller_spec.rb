923f09f0524b436d9dd9847203fdfea1fec8f322
sven <bohms@ideomatic.io>
2019-11-26 09:12:37 -0500

    test fixes

--- spec/controllers/materials_controller_spec.rb
+++ spec/controllers/materials_controller_spec.rb
@@ -3,121 +3,124 @@
 describe MaterialsController, type: :controller do
   render_views
 
-  let(:material) { FactoryBot.create(:material, name: 'custom material') }
+  context 'normal user' do
+    let(:company) { find_or_factory(:company, name: 'lter') }
+    let(:material) { find_or_factory(:material, name: 'custom material', company: company) }
 
-  before :each do
-    allow(material).to receive(:save).and_return(true)
-    allow(Material).to receive(:persisted?).and_return(true)
-    allow(Material).to receive(:find).with(material.id.to_s).and_return(material)
-    allow(Material).to receive(:by_company).and_return(Material)
+    before(:example) do
+      allow(material).to receive(:save).and_return(true)
+      allow(Material).to receive(:persisted?).and_return(true)
+      allow(Material).to receive(:find).with(material.id.to_s).and_return(material)
+      allow(Material).to receive(:by_company).and_return(Material)
 
-    sign_in_as_normal_user
-  end
-
-  it 'should get index' do
-    get :index
-    assert_response :success
-    assert assigns(:materials)
-  end
-
-  describe 'GET new' do
-    it 'should render new' do
-      get :new
-      expect(response).to render_template :new
-    end
-  end
-
-  describe 'POST: create with valid attributes' do
-    before(:each) do
-      post :create, params: { material: { name: 'NewName' } }
+      sign_in_as_normal_user
     end
 
-    it 'create the material' do
-      expect(Material.exists?(assigns[:material].id)).to eq true
+    it 'should get index' do
+      get :index
+      assert_response :success
+      assert assigns(:materials)
     end
 
-    it 'redirects to new material' do
-      expect(response).to redirect_to material_url(assigns(:material))
-    end
-  end
-
-  describe 'POST :create with invalid attributes' do
-    before(:each) do
-      post :create, params: { material: { name: '' } }
+    describe 'GET new' do
+      it 'should render new' do
+        get :new
+        expect(response).to render_template :new
+      end
     end
 
-    it { should render_template :new }
-    it { should_not set_flash }
-  end
+    describe 'POST: create with valid attributes' do
+      before(:each) do
+        post :create, params: { material: { name: 'NewName' } }
+      end
 
-  describe 'POST :create with XML format' do
-    before(:each) do
-      post :create, params: { material: { name: 'xml_name' }, format: 'xml' }
+      it 'create the material' do
+        expect(Material.exists?(assigns[:material].id)).to eq true
+      end
+
+      it 'redirects to new material' do
+        expect(response).to redirect_to material_url(assigns(:material))
+      end
     end
 
-    it { should respond_with(201) }
-    it 'should respond with content type application/xml' do
-      expect(response.content_type).to eq 'application/xml'
-    end
-  end
+    describe 'POST :create with invalid attributes' do
+      before(:each) do
+        post :create, params: { material: { name: '' } }
+      end
 
-  describe 'GET show' do
-    before(:each) do
-      expect(Material).to receive(:find_with_children).with(material.id.to_s).and_return(material)
-      get :show, params: { id: material }
+      it { should render_template :new }
+      it { should_not set_flash }
     end
 
-    it 'renders the show template' do
-      expect(response).to render_template :show
+    describe 'POST :create with XML format' do
+      before(:each) do
+        post :create, params: { material: { name: 'xml_name' }, format: 'xml' }
+      end
+
+      it { should respond_with(201) }
+      it 'should respond with content type application/xml' do
+        expect(response.content_type).to eq 'application/xml'
+      end
     end
 
-    it 'should assign the right material to @material' do
-      expect(assigns(:material)).to eq material
-    end
-  end
+    describe 'GET show' do
+      before(:each) do
+        expect(Material).to receive(:find_with_children).with(material.id.to_s).and_return(material)
+        get :show, params: { id: material }
+      end
 
-  describe 'GET #edit' do
-    it 'assigns the right material to @material' do
-      get :edit, params: { id: material }
-      expect(assigns(:material)).to eq material
-    end
+      it 'renders the show template' do
+        expect(response).to render_template :show
+      end
 
-    it 'renders template edit' do
-      get :edit, params: { id: material }
-      expect(response).to render_template :edit
-    end
-  end
-
-  it 'should update material' do
-    put :update, params: { id: material, material: { name: 'updated_name' } }
-    assert_redirected_to material_path(assigns(:material))
-  end
-
-  describe 'PUT :update with invalid attributes' do
-    before(:each) do
-      allow(material).to receive(:update_attributes).and_return(false)
-      put :update, params: { id: material, material: { name: 'repeat_name' } }
+      it 'should assign the right material to @material' do
+        expect(assigns(:material)).to eq material
+      end
     end
 
-    it 'assigns to the right material' do
-      expect(assigns(:material)).to eq material
+    describe 'GET #edit' do
+      it 'assigns the right material to @material' do
+        get :edit, params: { id: material }
+        expect(assigns(:material)).to eq material
+      end
+
+      it 'renders template edit' do
+        get :edit, params: { id: material }
+        expect(response).to render_template :edit
+      end
     end
 
-    it { should render_template :edit }
-  end
-
-  describe 'DESTROY' do
-    before(:each) do
-      # allow(material).to receive(:destroy).and_return(true)
-      delete :destroy, params: { id: material }
+    it 'should update material' do
+      put :update, params: { id: material, material: { name: 'updated_name' } }
+      assert_redirected_to material_path(assigns(:material))
     end
 
-    it 'should destroy material' do
-      expect(Material.exists?(material.id)).to eq false
+    describe 'PUT :update with invalid attributes' do
+      before(:each) do
+        allow(material).to receive(:update_attributes).and_return(false)
+        put :update, params: { id: material, material: { name: 'repeat_name' } }
+      end
+
+      it 'assigns to the right material' do
+        expect(assigns(:material)).to eq material
+      end
+
+      it { should render_template :edit }
     end
 
-    it 'redirects to index' do
-      expect(response).to redirect_to materials_url
+    describe 'DESTROY' do
+      before(:each) do
+        # allow(material).to receive(:destroy).and_return(true)
+        delete :destroy, params: { id: material }
+      end
+
+      it 'should destroy material' do
+        expect(Material.exists?(material.id)).to eq false
+      end
+
+      it 'redirects to index' do
+        expect(response).to redirect_to materials_url
+      end
     end
   end
 end
--- spec/models/material_transaction_spec.rb
+++ spec/models/material_transaction_spec.rb
@@ -5,16 +5,16 @@ describe MaterialTransaction do
       @transaction = MaterialTransaction.new
     end
 
-    context "180 lb of 46-0-0 " do
+    context '180 lb of 46-0-0 ' do
       before(:each) do
         @transaction.rate = 180.0
-        @material = find_or_factory(:material, :name => 'n_content_material', :n_content => 46)
-        @unit = find_or_factory(:unit, :conversion_factor => 453.592)
+        @material = FactoryBot.build(:material, name: 'n_content_material', n_content: 46)
+        @unit = FactoryBot.build(:unit, conversion_factor: 453.592)
         @transaction.material = @material
         @transaction.unit = @unit
       end
 
-      it "provides 92.7 kg N/ha " do
+      it 'provides 92.7 kg N/ha ' do
         assert_equal 92.77, @transaction.n_content_to_kg_ha
       end
     end
@@ -22,8 +22,9 @@ describe MaterialTransaction do
     describe '16 gal of 28-0-0 ' do
       before do
         @transaction.rate = 16
-        @material = find_or_factory(:material, name: 'n_content_material', n_content: 28, liquid: true, specific_weight: 1.28)
-        @unit = find_or_factory(:unit, :conversion_factor => 3785.41)
+        @material = FactoryBot.build(:material, name: 'n_content_material', n_content: 28,
+                                                liquid: true, specific_weight: 1.28)
+        @unit = FactoryBot.build(:unit, conversion_factor: 3785.41)
         @transaction.material = @material
         @transaction.unit = @unit
       end
@@ -32,69 +33,69 @@ describe MaterialTransaction do
       end
     end
 
-    context "14 lb/A of P2O5 " do
+    context '14 lb/A of P2O5 ' do
       before(:each) do
         @transaction.rate = 14.0
-        @material = find_or_factory(:material, :name => 'p_content_material', :p_content => 43.64)
-        @unit = find_or_factory(:unit, :conversion_factor => 453.592)
+        @material = FactoryBot.build(:material, name: 'p_content_material', p_content: 43.64)
+        @unit = FactoryBot.build(:unit, conversion_factor: 453.592)
         @transaction.material = @material
         @transaction.unit = @unit
       end
 
-      it "provides 6.85 kg P/ha" do
+      it 'provides 6.85 kg P/ha' do
         assert_equal 6.85, @transaction.p_content_to_kg_ha
       end
     end
 
-    context "120 lb of K2O/A " do
+    context '120 lb of K2O/A ' do
       before(:each) do
         @transaction.rate = 120
-        @material = find_or_factory(:material, :name => 'k_content_material', :k_content => 49.8)
-        @unit = find_or_factory(:unit, :conversion_factor => 453.592)
+        @material = FactoryBot.build(:material, name: 'k_content_material', k_content: 49.8)
+        @unit = FactoryBot.build(:unit, conversion_factor: 453.592)
         @transaction.material = @material
         @transaction.unit = @unit
       end
 
-      it "provides 66.95 kg K/ha" do
+      it 'provides 66.95 kg K/ha' do
         assert_equal 66.95, @transaction.k_content_to_kg_ha
       end
     end
 
     context 'the transaction involves seeds ' do
       it 'gets the right rate when provided with seeds per acre' do
-        @transaction.rate = 30000
-        material = find_or_factory(:material, name: 'seed')
-        unit = find_or_factory(:unit, name: "seeds")
+        @transaction.rate = 30_000
+        material = FactoryBot.build(:material, name: 'seed')
+        unit = FactoryBot.build(:unit, name: 'seeds')
         @transaction.material = material
         @transaction.unit = unit
         expect(@transaction.seeds_per_square_meter).to eq 7.41
       end
     end
 
-    context "The transaction has a unit. " do
+    context 'The transaction has a unit. ' do
       before(:each) do
-        @transaction.unit = find_or_factory(:unit, :name => 'barrel')
-        @transaction.material = find_or_factory(:material, :name => "Beets")
+        @transaction.unit = FactoryBot.build(:unit, name: 'barrel')
+        @transaction.material = FactoryBot.build(:material, name: 'Beets')
       end
 
       it 'returns a material and rate on material_with_rate' do
         @transaction.rate = 4
-        assert_equal "Beets: 4.0 barrels per acre", @transaction.material_with_rate
+        assert_equal 'Beets: 4.0 barrels per acre', @transaction.material_with_rate
 
         @transaction.rate = 1
-        assert_equal "Beets: 1.0 barrel per acre",  @transaction.material_with_rate
+        assert_equal 'Beets: 1.0 barrel per acre', @transaction.material_with_rate
       end
     end
 
-    context "The transaction does not have a unit. " do
+    context 'The transaction does not have a unit. ' do
       before(:each) do
         @transaction.unit = nil
-        @transaction.material = find_or_factory(:material, :name => "Carrots")
+        @transaction.material = FactoryBot.build(:material, name: 'Carrots')
       end
 
       it 'returns just material on material_with_rate' do
         @transaction.rate = 4
-        assert_equal "Carrots", @transaction.material_with_rate
+        assert_equal 'Carrots', @transaction.material_with_rate
       end
     end
   end
