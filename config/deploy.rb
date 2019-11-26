d662be8591b5cadbae3da4545cf329a27dcd0cb9
sven <bohms@ideomatic.io>
2019-11-25 20:34:03 -0500

    updated for webpacker

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-lock '3.11.0'
+# lock '3.11.2'
 
 set :application, 'aglog'
 set :repo_url, 'https://github.com/kf8a/aglog.git'
@@ -21,12 +21,24 @@ set :deploy_to, '/var/u/apps/aglog'
 set :linked_files, %w[config/database.yml .env]
 
 # Default value for linked_dirs is []
-set :linked_dirs, %w[log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads]
+set :linked_dirs, %w[log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads public/packs node_modules]
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
 # Default value for keep_releases is 5
-# set :keep_releases, 5
+set :keep_releases, 10
 
 after 'deploy:publishing', 'unicorn:restart'
+
+before 'deploy:assets:precompile', 'deploy:yarn_install'
+namespace :deploy do
+  desc 'Run rake yarn install'
+  task :yarn_install do
+    on roles(:web) do
+      within release_path do
+        execute("cd #{release_path} && yarn install --silent --no-progress --no-audit --no-optional")
+      end
+    end
+  end
+end
