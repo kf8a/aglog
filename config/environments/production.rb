eba625d656af0442194f6ce7928443a1c402bc83
sven <bohms@ideomatic.io>
2020-01-13 11:36:20 -0500

    gem version bump

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,7 +10,7 @@ Aglog::Application.configure do
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
   # config.serve_static_files  = false
-  config.serve_static_files  = true
+  config.serve_static_files = true
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
@@ -40,7 +40,7 @@ Aglog::Application.configure do
   config.log_level = :info
 
   # Prepend all log lines with the following tags
-  config.log_tags = [ :subdomain, :uuid ]
+  config.log_tags = %i[subdomain uuid]
 
   # Use a different logger for distributed setups
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
@@ -62,7 +62,7 @@ Aglog::Application.configure do
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found)
-  #config.i18n.fallbacks = true
+  config.i18n.fallbacks = [I18n.default_locale]
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
