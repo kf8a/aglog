d87adc2df478206a4e9d322bc0890e12cf756284
sven <bohms@ideomatic.io>
2020-01-21 09:07:18 -0500

    set global mass assigment to false

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,4 +61,5 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.log_level = :debug
+  config.active_record.whitelist_attributes = true
 end
