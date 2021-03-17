1940bad7fffae964e83eb86e9bd6f485d33cc66c
sven <bohms@ideomatic.io>
2021-03-17 09:50:10 -0400

    style fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,16 @@
 # frozen_string_literal: true
 
 # An account in the system
-class User < ActiveRecord::Base # :timeoutable and :omniauthable # :token_authenticatable, :encryptable, :confirmable, :lockable, # Include default devise modules. Others available are:
+# :timeoutable and :omniauthable
+#  :token_authenticatable, :encryptable, :confirmable, :lockable,
+#  Include default devise modules. Others available are:
+#  devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable
+
+class User < ActiveRecord::Base
   devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable
 
   # belongs_to :company
-  has_one :person
+  has_one :person, dependent: :nullify
 
   delegate :companies, to: :person
 
--- app/models/wps.rb
+++ app/models/wps.rb
@@ -3,9 +3,11 @@
 # Worker Protection Standard helpers
 class Wps
   def self.closed_fields
-    ObservationType.find(5).observations.where(company_id: 1).where('obs_date > ?', Date.today - 30.days).order( # 5 is the pesticide observation type
-      'obs_date'
-    ).collect { |record| records(record) }.flatten
+    # 5 is the pesticide observation type 'obs_date'
+    ObservationType.find(5).observations.where(company_id: 1)
+      .where('obs_date > ?', Time.zone.today - 30.days)
+      .order
+      .collect { |record| records(record) }.flatten
   end
 
   def self.records(record)
