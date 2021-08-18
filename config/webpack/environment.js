5cc62feb2d98402b9618936a8e1f2d17e2ea3ac3
sven <bohms@ideomatic.io>
2021-08-18 09:16:15 -0400

    add jquery back in

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,11 @@
 const elm =  require('./loaders/elm')
 const { environment } = require('@rails/webpacker')
 
+environment.plugins.append('Provide', new webpack.ProvidePlugin({
+  $: 'jquery',
+  jQuery: 'jquery',
+  Popper: ['popper.js', 'default']
+}));
+
 environment.loaders.prepend('elm', elm)
 module.exports = environment
