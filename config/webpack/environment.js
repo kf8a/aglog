16ad534b7189380fdc2e30e5b359eb042f791c34
sven <bohms@ideomatic.io>
2021-08-18 10:04:50 -0400

    fixed webpacker configuration. I had refered to webpack in the configuration without definining it first. The error message got buried in the output from the page load

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,11 +1,18 @@
-const elm =  require('./loaders/elm')
 const { environment } = require('@rails/webpacker')
+const elm =  require('./loaders/elm')
+const webpack = require('webpack')
 
 environment.plugins.append('Provide', new webpack.ProvidePlugin({
   $: 'jquery',
   jQuery: 'jquery',
   Popper: ['popper.js', 'default']
 }));
+
+// const config = environment.toWebpackConfig();
+//
+// config.resolve.alias = {
+//  jquery: 'jquery/src/jquery'
+// };
 
 environment.loaders.prepend('elm', elm)
 module.exports = environment
