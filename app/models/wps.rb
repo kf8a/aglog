07d607789e0b9f93c5d179ce0bbe653ea6c97677
Sven Bohm <bohms@hey.com>
2022-04-26 16:22:15 -0400

    add a herbicide application test to wps

--- app/models/wps.rb
+++ app/models/wps.rb
@@ -4,10 +4,17 @@
 class Wps
   def self.closed_fields
     # 5 is the pesticide observation type 'obs_date'
-    ObservationType.find(5).observations.where(company_id: 1)
+    # 13 is a herbicde application
+    pesticde = ObservationType.find(5).observations.where(company_id: 1)
       .where('obs_date > ?', Time.zone.today - 30.days)
       .order('obs_date')
       .collect { |record| records(record) }.flatten
+
+    herbicde = ObservationType.find(13).observations.where(company_id: 1)
+      .where('obs_date > ?', Time.zone.today - 30.days)
+      .order('obs_date')
+      .collect { |record| records(record) }.flatten
+    [pesticde, herbicde].flatten.uniq
   end
 
   def self.records(record)
