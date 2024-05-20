c32dc42be341a0c7a695d3db77622fe25ea8351f
Sven Bohm <bohms@hey.com>
2024-05-20 13:55:32 -0400

    load version 6 defaults

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Aglog
   class Application < Rails::Application # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.0
+    config.load_defaults 6.1
 
     config.generators.javascript_engine = :js
 
